410b026345533b2496efb51be8a0354399c99fd2
greyCimath <grey.qisen@gmail.com>
2019-03-29 18:38:24 +0800

    20190329_JavaException

---
+++ Java_L/className.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class className{
+    public void deposit(double amount) throws RemoteException{
+        // method implementation
+        throw new RemoteException();
+    }
+}
